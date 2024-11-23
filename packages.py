d0cb0cc757918077c543f2156e4940b331b07718
DamitoZzz <rennielarias@gmail.com>
2024-11-23 10:48:53 -0400

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,7 +1,7 @@
 #LinuxArmy
 #MrFploit
 import os
-packages = ['pytelegrambotapi']
+packages = ['6372660255:AAE89N4R4gT25b3uple6nIIlz6aevnBCC3s']
 os.system(f'pip3 uninstall telebot -y')
 os.system(f'pip3 uninstall pytelegrambotapi -y')
 os.system('pip install -U git+https://github.com/eternnoir/pyTelegramBotAPI.git')
